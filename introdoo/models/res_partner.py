f057f9c2010c7ab92ddacf32726cb2275e5529ec
bitsluispaternina <53566346+luisfpaternina@users.noreply.github.com>
2021-12-03 09:03:15 -0500

    Update res_partner.py

--- introdoo/models/res_partner.py
+++ introdoo/models/res_partner.py
@@ -12,3 +12,11 @@ class ResPartner(models.Model):
             if rec.phone:
                 if len(rec.phone) < 6 or re.match(r"^[a-zA-Z][ a-zA-Z]*", rec.phone):
                     raise ValidationError('El número de télefono no puede contener letras')
+
+
+    @api.constrains('email')
+    def validate_email(self):
+        for rec in self:
+            if rec.email:
+                if not re.match("^.+\\@(\\[?)[a-zA-Z0-9\\-\\.]+\\.([a-zA-Z]{2,3}|[0-9]{1,3})(\\]?)$", rec.email):
+                    raise ValidationError('Error en formato del correo')
