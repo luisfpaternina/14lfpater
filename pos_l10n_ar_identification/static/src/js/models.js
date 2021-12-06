a7d828e4c28966641bb6def389a92f2c309b1fb7
bitsluispaternina <53566346+luisfpaternina@users.noreply.github.com>
2021-12-06 08:56:09 -0500

    Update models.js

--- pos_l10n_ar_identification/static/src/js/models.js
+++ pos_l10n_ar_identification/static/src/js/models.js
@@ -5,34 +5,4 @@ odoo.define('pos_l10n_ar_identification.models', function (require) {
 
     models.load_fields('res.partner', ['first_name',
                                           'last_name']);
-
-
-    
-
-   models.load_models({
-            model: 'l10n_ar.afip.responsibility.type',
-            fields: [],
-            loaded: function (self, responsability_type) {
-            self.responsability_type=responsability_type;
-                
-            },
-        });
-
-    models.load_models([
-        {
-            model: 'l10n_latam.identification.type',
-            fields: [],
-            loaded: function (self, identification_type) {
-                self.identification_type = identification_type;
-            }
-        }
-    ]);
-
-
-
-
-
-
-   
-
 }); 
