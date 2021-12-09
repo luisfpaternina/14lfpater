37165eadc733a4887cc31415e6d1a2006828f0c5
bitsluispaternina <53566346+luisfpaternina@users.noreply.github.com>
2021-12-09 16:12:24 -0500

    add

--- pos_l10n_ar_identification/static/src/js/models.js
+++ pos_l10n_ar_identification/static/src/js/models.js
@@ -3,9 +3,36 @@ odoo.define('pos_l10n_ar_identification.models', function (require) {
 
     var models = require('point_of_sale.models');
 
-    models.load_fields('res.partner', ['first_name',
-                                          'last_name']);
+    models.load_fields('res.partner', ['l10n_ar_afip_responsibility_type_id',
+                                          'l10n_latam_identification_type_id']);
+
+
     
-    console.log("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
-    console.log(models)
+
+   models.load_models({
+            model: 'l10n_ar.afip.responsibility.type',
+            fields: [],
+            loaded: function (self, responsability_type) {
+            self.responsability_type=responsability_type;
+                
+            },
+        });
+
+    models.load_models([
+        {
+            model: 'l10n_latam.identification.type',
+            fields: [],
+            loaded: function (self, identification_type) {
+                self.identification_type = identification_type;
+            }
+        }
+    ]);
+
+
+
+
+
+
+   
+
 }); 
