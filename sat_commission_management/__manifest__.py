1a25fb9d0d3443f18e5980b7e627ac02f50527c3
bitsluispaternina <53566346+luisfpaternina@users.noreply.github.com>
2021-12-28 05:48:25 -0500

    add

---
+++ sat_commission_management/__manifest__.py
@@ -0,0 +1,29 @@
+{
+    'name': 'sat commission management',
+
+    'version': '14.0.1',
+
+    'author': "Process Control",
+
+    'contributors': ['Luis Felipe Paternina'],
+
+    'website': "https://www.processcontrol.es/",
+
+    'category': 'commission',
+
+    'depends': [
+
+        'sale_management',
+        'base',
+
+    ],
+
+    'data': [
+       
+        #'security/security.xml',
+        #'security/ir.model.access.csv',
+        #'views/account_move.xml',
+                   
+    ],
+    'installable': True
+}
